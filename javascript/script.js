19f3d02484d0f9163d393d4f7318794a93d3cc0a
wassimmohammedi <wassim.mohammedi720@gmail.com>
2025-03-22 17:44:17 +0100

    some changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,9 @@
- 
- function scrollTo(id) {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "start" });
+function scrollToElement(selector) {
+    const element = document.querySelector(selector);
+
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.error(`Element with selector "${selector}" not found.`);
+    }
 }
-
-function toggleNavbar() {
-let navbar = document.getElementById("navbare");
-navbar.classList.toggle("active");
-}
-let lastScrollTop = 0;
-const navbar = document.querySelector("nav");
-
-window.addEventListener("scroll", function() {
-const currentScroll = window.scrollY;
-
-// Always show navbar when at the top of the page
-if (currentScroll <= 0) {
-navbar.classList.remove("nav-hidden");
-} else {
-// Hide on scroll down, show on scroll up
-if (currentScroll > lastScrollTop) {
-    navbar.classList.add("nav-hidden");
-} else {
-    navbar.classList.remove("nav-hidden");
-}
-}
-
-lastScrollTop = currentScroll;
-});
-
